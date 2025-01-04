5c2b347b60e789c6fa076c0d4615c7e82e25298a
Babu Natarajan <babu.natarajan@gmail.com>
2025-01-04 19:50:49 +0100

    Add files via upload

---
+++ com.babu.gha/src/com/babu/gha/Main.java
@@ -0,0 +1,10 @@
+package com.babu.gha;
+
+public class Main {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World...");
+
+	}
+
+}
---
+++ com.babu.gha/src/module-info.java
@@ -0,0 +1,2 @@
+module com.babu.gha {
+}
